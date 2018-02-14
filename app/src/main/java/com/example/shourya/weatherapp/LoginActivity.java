9540165bec9c296b536a7f57064058e862c7da29
Shourya Sharma <shourya30@gmail.com>
2018-02-14 16:05:10 +0530

    Dynamic Background

--- app/src/main/java/com/example/shourya/weatherapp/LoginActivity.java
+++ app/src/main/java/com/example/shourya/weatherapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.shourya.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Dynamic Background
+        getWindow().setBackgroundDrawableResource(R.drawable.blurbeachbackground) ;
     }
 
 }
